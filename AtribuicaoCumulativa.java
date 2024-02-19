8aab23ab7a75b810615db2fe654933655f87d0e7
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-19 11:15:29 -0300

    Operações de Atribuição Cumulativa (a += 1)

---
+++ AtribuicaoCumulativa.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AtribuicaoCumulativa {
+    public static void main(String[] args) {
+      Locale.setDefault(Locale.US);
+      Scanner input = new Scanner(System.in);
+      a += b // a = a + b
+      a -= b // a = a - b
+      a *= b // a = a * b
+      a /= b // a = a / b
+      a %= b // a = a % b
+      input.close();
+    }
+}
