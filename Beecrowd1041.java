40a9be56bdf9ee94a6b5a528d4a60d959e9d2d51
LucasTheHacker <106922188+LucasTheHacker@users.noreply.github.com>
2024-02-19 10:37:42 -0300

    Seguir o comando, não a lógica.

--- Beecrowd1041.java
+++ Beecrowd1041.java
@@ -1,42 +1,53 @@
-package dr.lucas.batista.codigosjava;
-
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Beecrowd1041 {
-    public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
 
-        double x,y;
+		double x, y;
 
-        x = input.nextDouble();
-        y = input.nextDouble();
+		x = input.nextDouble();
+		y = input.nextDouble();
 
-        if (x == 0 && y == 0) {
-            System.out.println("Origem");
-        }
-        else {
-            if (x == 0) {
-                System.out.println("Eixo Y");
-            }
-            if (y == 0) {
-                System.out.println("Eixo X");
-            }
-            if (x > 0 && y > 0) {
-                System.out.println("Q1");
-            }
-            if (x > 0 && y < 0) {
-                System.out.println("Q2");
-            }
-            if (x < 0 && y < 0) {
-                System.out.println("Q3");
-            }
-            if (x < 0 && y > 0) {
-                System.out.println("Q4");
-            }
-        }
-        input.close();
-    }
-    
+		if (x > 0 && y > 0) {
+			System.out.println("Q1");
+		}
+		else if (x < 0 && y > 0) {
+			System.out.println("Q2");
+		}
+		else if (x < 0 && y < 0) {
+			System.out.println("Q3");
+		}
+		else if (x > 0 && y < 0) {
+			System.out.println("Q4");
+		}
+		else if (x == 0 && y == 0) {
+			System.out.println("Origem");
+		}
+		else if (x == 0) {
+			System.out.println("Eixo Y");
+		}
+		else if (y == 0) {
+			System.out.println("Eixo X");
+		}
+		input.close();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
